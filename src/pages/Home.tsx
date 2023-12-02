5d51d6c536d16f671ea87241b069b3ed24eb42c8
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-12-02 17:33:44 +0530

    working code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,10 +24,10 @@ const Home = () => {
       ) : (
       <div className="space-y-10 pb-10">
         <div className="p-4 sm:p-6 lg:p-8 rounded-lg overflow-hidden">
-          <div
-            className={`bg-[url('img/hero-banner.jpg')] rounded-lg relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-center`}
-            onLoad={() => setImgLoaded(true)}
-          >
+        <div className={`rounded-lg relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-center`}
+          style={{ backgroundImage: "url('/img/hero-banner.jpg')" }}
+          onLoad={() => setImgLoaded(true)}
+      >
             {!imgLoaded && <Skeleton />}
             <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
               <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs text-black dark:text-white bg-secondary/60 p-4 rounded-lg">
