ed8fee719fbf7eaf53c0ae835678cd771e861d27
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-12-04 19:03:19 +0530

    teambuilderpro

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -3,6 +3,7 @@ import fetcher from '@/lib/fetcher';
 
 const useUsers = (page: number, limit: number) => {
     const { data, error } = useSwr(`${import.meta.env.VITE_APP_API_BASE_URL}/api/users?page=${page}&limit=${limit}`, fetcher);
+    console.log(data);
     
     return {
         users: data ? data.users : [],
